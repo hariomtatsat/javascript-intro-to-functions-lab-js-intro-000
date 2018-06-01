7147224a0949af9b33992abd0e17edf6a363b0be
Hariom Desai <hariom.m.desai@gmail.com>
2018-06-01 20:09:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@
       return "I can't hear you!"
     } else if (string === string.toUpperCase()) {
       return "YES INDEED!"
-    } else if (string === "I love you, Gandma."){
+    } else if (string === "I love you, Grandma."){
       return "I love you, too."
     }
   }
