8f0f34ac6f9abfcd74907e1715cce368da42cf47
Hariom Desai <hariom.m.desai@gmail.com>
2018-06-01 20:02:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,14 @@
 
   function logShout(string){
     console.log( string.toUpperCase())
+  }
 
+  function logWhisper(string){
+    console.log( string.toLowerCase())
+  }
+
+  function sayHiToGrandma(string){
+    if (string === string.toLowerCase()){
+      return "I can't hear you!"
+    }
   }
