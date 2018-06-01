25707fc60feaac30fb072e6065bd8933ea099b9e
Hariom Desai <hariom.m.desai@gmail.com>
2018-06-01 20:03:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,9 @@
   function sayHiToGrandma(string){
     if (string === string.toLowerCase()){
       return "I can't hear you!"
+    } elseif ( string === string.toUpperCase()){
+      return "YES INDEED!"
+    } elseif (string === "I love you, Gandma."){
+      return "I love you, too."
     }
   }
